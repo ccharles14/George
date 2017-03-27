0192ce44bcf19e18b1a87a93e2752145bc047600
ccharles14 <corentin.charles@groupe-esigelec.org>
2017-03-27 02:24:36 +0200

    Correction commentaires

--- Uart.c
+++ Uart.c
@@ -22,12 +22,15 @@ void InitUART(void)
     IE2 |= UCA0RXIE;                        // Enable USCI_A0 RX interrupt
 }
 
+//Fonction pour envoyer une donée via l'Uart.
 void TXdata( unsigned char c )
 {
 	while (!(IFG2&UCA0TXIFG));  // USCI_A0 TX buffer ready?
     UCA0TXBUF = c;              // TX -> RXed character
 }
 
+
+//Fonction pour recevoir une donnée via l'Uart.
 unsigned char RXdata(void){
 	unsigned char a;
 	while (!(IFG2&UCA0RXIFG));
@@ -35,6 +38,7 @@ unsigned char RXdata(void){
 	return a;
 }
 
+//En cas de demande d'aide de la part de l'utilisateur, ce texte sera affiché sur n'importe quelle interface.
 void help(void){
 	unsigned char h[89]={'C','o','m','m','a','n','d',' ','s','i','m','p','l','e',':','\n','\r', 	//Commande simple:
 						'a','v','a','n','c','e','r',':',' ','z','\n','\r', 							//avancer: z
